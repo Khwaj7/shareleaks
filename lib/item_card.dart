e5989230e9b0bec7024cb1b721d4f23cfd1adbd8
Khwaj7 <khwaja7@proton.me>
2022-09-08 16:52:39 +0200

    New item card layout

--- lib/item_card.dart
+++ lib/item_card.dart
@@ -1,32 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
+import 'item_card_data.dart';
+
+final Uri _url = Uri.parse('https://flutter.dev');
+
+@immutable
 class ItemCard extends StatelessWidget {
-  const ItemCard({data: String});
+  final ItemCardData data;
+
+  const ItemCard({Key? key, required this.data}) : super(key: key);
+
+  Future<void> _launchUrl() async {
+    if (!await launchUrl(Uri.parse(data.originUrl))) {
+      throw 'Could not launch $Uri.parse(data.originUrl)';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 400,
-      width: 400,
-      child: Card(
-        clipBehavior: Clip.antiAlias,
-        child: Column(
-          children: [
-            ListTile(
-              leading: const Icon(Icons.arrow_drop_down_circle),
-              title: const Text('Card title 1'),
-              subtitle: Text(
-                'Secondary Text',
-                style: TextStyle(color: Colors.black.withOpacity(0.6)),
-              ),
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      child: Column(
+        children: [
+          ListTile(
+            leading: const Icon(Icons.person_pin),
+            title: Text(data.creatorName),
+            subtitle: InkWell(
+              onTap: _launchUrl,
+              child: Text('source: ${data.origin}',
+                  style: const TextStyle(color: Colors.blue)),
             ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                'Greyhound divisively hello coldly wonderfully marginally far upon excluding.',
-                style: TextStyle(color: Colors.black.withOpacity(0.6)),
-              ),
-            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.asset('assets/card-sample-image-2.jpg'),
+          ),
+          Row(
+            children: [
+              const Padding(padding: EdgeInsets.all(8.0)),
+              Text(
+                  textAlign: TextAlign.left,
+                  'Added on ${data.uploadDate.year}-${data.uploadDate.month}-${data.uploadDate.day}',
+                  style: TextStyle(color: Colors.black.withOpacity(0.6))),
+              const Spacer(),
+              Text(
+                  textAlign: TextAlign.right,
+                  '${data.nbElements} elements',
+                  style: TextStyle(color: Colors.black.withOpacity(0.6))),
+              const Padding(padding: EdgeInsets.all(8.0)),
+            ],
+          ),
+
+          /*
+          Align(
+                    alignment: Alignment.centerLeft,
+                    child: Container(
+                      color: Colors.red,
+                      child: Text(
+                          textAlign: TextAlign.left,
+                          '${data.uploadDate.year}-${data.uploadDate.month}-${data.uploadDate.day}',
+                          style:
+                              TextStyle(color: Colors.black.withOpacity(0.6))),
+                    ),
+                  ),
+
+
+
             ButtonBar(
               alignment: MainAxisAlignment.start,
               children: [
@@ -47,9 +87,8 @@ class ItemCard extends StatelessWidget {
               ],
             ),
             //Image.asset('assets/card-sample-image.jpg'),
-            Image.asset('assets/card-sample-image-2.jpg'),
-          ],
-        ),
+            */
+        ],
       ),
     );
   }
--- lib/item_card_data.dart
+++ lib/item_card_data.dart
@@ -1,3 +1,16 @@
 class ItemCardData {
-  const ItemCardData({required String data});
+  final String creatorName;
+  final String origin;
+  final String originUrl;
+  final double nbElements;
+  final DateTime uploadDate;
+
+  late String avatar;
+
+  ItemCardData(
+      {required this.creatorName,
+      required this.origin,
+      required this.originUrl,
+      required this.nbElements,
+      required this.uploadDate});
 }
